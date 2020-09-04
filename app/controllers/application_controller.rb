39140cecb06d44e49a73c5d002ac734e5038f458
codekane <ryan.horricks@gmail.com>
2020-09-04 00:55:30 -0600

    Hook into YouTube Data API and pull Youtube Search Results, then populate a container with them. They don't go anywhere yet, and I'm hesitant to do further work on this feature until I can test some of these blatantly-obvious bugs. I figure, if I've gotta go bug-hunting in order to make the App suitable to be customer-facing anyway, I had may as well get a semi-reasonable testing suite out of it. I can see this App having an actual lifespan, so it warrants all of the efforts. It's still ugly layout-wise, but I'm doing the minimum of effort right now. Getting it actually functional is of a higher priority.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ require 'sinatra/base'
 class ApplicationController < Sinatra::Base
 
   configure do
+    enable :sessions
+    set :session_secret, "Dankhouse"
     set :views, "app/views"
     set :public_dir, "public"
   end
@@ -12,7 +14,16 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/queue' do
+    if session[:name]
+      @name = session[:name]
+    end
     erb :queue
     # "Hello World"
+  end
+
+  post '/register' do
+    @name = params["name"]
+    session[:name] = params["name"]
+    redirect "/queue"
   end
 end
---
+++ public/javascript/main.js
@@ -0,0 +1,83 @@
+document.getElementById("queue-popper").onclick = function(event) {
+  let target = document.getElementById("queue-pop");
+  if (target.style.display === "none") {
+    target.style.display = "block";
+  } else {
+    target.style.display = "none";
+  }
+}
+
+document.getElementById("searchSubmit").onclick = function(event) {
+  event.preventDefault();
+  let inputVal = document.getElementById("searchInput").value;
+  getVideos(inputVal);
+}
+
+// Hits the YouTube Data API to pull in a batch of search results, and populate 
+// the queue with cards.
+function getVideos(search) {
+  $.ajax({
+    type: 'GET',
+    url: 'https://www.googleapis.com/youtube/v3/search',
+    data: {
+      key: 'AIzaSyCwKyWI7WeJ6XPRUM4-9TrZsg0YKbk2zAI',
+      q: search,
+      part: 'snippet',
+      maxResults: 100,
+      type: 'video',
+      videoEmbeddable: true,
+    },
+    success: function(data) {
+      let target = document.getElementById("searchResults");
+      for (let item in data.items) {
+        // Helper to find where we are.
+        var thisItem = data.items[item];
+
+        // Creates the search card itself
+        var card = document.createElement("div");
+        card.className = "search-card";
+
+        // Create the image to go inside the card
+        var thisImg = thisItem.snippet.thumbnails.default;
+        var image = document.createElement("img");
+        image.src = thisImg.url;
+        image.width = thisImg.width;
+        image.height = thisImg.height;
+        card.appendChild(image);
+
+        // Creates the container for the rest of the card
+        var container = document.createElement("div");
+        container.className = "container";
+
+        // Adds the title row to that container
+        var titleRow = document.createElement("div");
+        titleRow.className = "row";
+
+        // Create the title
+        var title = document.createElement("b");
+        title.innerHTML = thisItem.snippet.title;
+        titleRow.appendChild(title);
+        container.appendChild(titleRow);
+
+        // Creates the 2nd (description) row
+        var descriptionRow = document.createElement("div");
+        descriptionRow.className = "row";
+
+        // Create the description
+        var description = document.createElement("p");
+        description.innerHTML = thisItem.snippet.description;
+        descriptionRow.appendChild(description);
+        container.appendChild(descriptionRow);
+
+        console.log(container);
+        card.appendChild(container);
+
+        // Adds the completed card to the queue list
+        target.appendChild(card);
+      }
+    },
+    error: function(response) {
+      console.log("Request Failed");
+    }
+  });
+}
